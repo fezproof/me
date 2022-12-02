92f440340d3919db3e0ad9113959a56cd9b980df
fezproof <26830309+fezproof@users.noreply.github.com>
2022-12-02 12:21:24 +0800

    INIT

---
+++ apps/dnd-cards/vite.config.ts
@@ -0,0 +1,11 @@
+import cloudflareWorkers from "solid-start-cloudflare-workers";
+import solid from "solid-start/vite";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  plugins: [
+    solid({
+      adapter: cloudflareWorkers({}) as any,
+    }),
+  ],
+});
