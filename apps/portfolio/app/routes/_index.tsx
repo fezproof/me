3ab23ebf62986f2e9e79c4e851f259b4ae7269e2
fezproof <26830309+fezproof@users.noreply.github.com>
2024-04-27 01:06:57 +0800

    Add octokit

--- apps/portfolio/app/routes/_index.tsx
+++ apps/portfolio/app/routes/_index.tsx
@@ -1,3 +1,21 @@
+import type { LoaderFunctionArgs } from "@remix-run/cloudflare";
+
+export const loader = async ({
+  request,
+  context,
+  params,
+}: LoaderFunctionArgs) => {
+  const res = await context.octokit.request(
+    "GET /repos/{owner}/{repo}/commits",
+    {
+      owner: "fezproof",
+      repo: "me",
+      path: "apps/blog",
+    }
+  );
+  return res.data.map((d) => d.commit.committer?.date);
+};
+
 export default function Index() {
   return (
     <div className="flex-1 px-2 md:px-4 max-w-xl mx-auto w-full">
--- apps/portfolio/load-context.ts
+++ apps/portfolio/load-context.ts
@@ -1,3 +1,5 @@
+import type { AppLoadContext } from "@remix-run/cloudflare";
+import { Octokit } from "octokit";
 import type { PlatformProxy } from "wrangler";
 
 type Cloudflare = Omit<PlatformProxy<Env>, "dispose">;
@@ -5,5 +7,20 @@ type Cloudflare = Omit<PlatformProxy<Env>, "dispose">;
 declare module "@remix-run/cloudflare" {
   interface AppLoadContext {
     cloudflare: Cloudflare;
+    octokit: Octokit;
   }
 }
+
+type GetLoadContext = (args: {
+  request: Request;
+  context: { cloudflare: Cloudflare }; // load context _before_ augmentation
+}) => AppLoadContext;
+
+// Shared implementation compatible with Vite, Wrangler, and Cloudflare Pages
+export const getLoadContext: GetLoadContext = ({ context }) => {
+  const octokit = new Octokit();
+  return {
+    ...context,
+    octokit,
+  };
+};
